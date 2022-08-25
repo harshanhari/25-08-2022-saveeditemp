1c63f5bc984473e823a2fa670fbd05234cd94412
harshanhari <107249324+harshanhari@users.noreply.github.com>
2022-08-25 13:10:45 +0530

    Add files via upload

---
+++ emp/DatabaseConnection.java
@@ -0,0 +1,9 @@
+package emp;
+
+public class DatabaseConnection {
+	public static final String driverClass = "com.mysql.jdbc.Driver";
+
+	public static final String connectionUrl = "jdbc:mysql://192.168.18.245:3306/javadb_167";
+	public static final String username = "javadb_167";
+	public static final String password = "ben#u62000";
+}
